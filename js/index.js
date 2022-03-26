4435ef2ef4bcbb912d5ab2eeee8182fc30b2d8f0
AtienoObwanda <atienoobwanda@gmail.com>
2022-03-26 13:11:01 +0300

    Added Navigation bar

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//ON SCROLL NAV BAR
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    
+    document.getElementById("navbar").style.background = "#ffffff";
+  } else {
+   
+    document.getElementById("navbar").style.background = "none";
+  }
+}
