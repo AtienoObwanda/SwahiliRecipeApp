d67425581fe7ffb8c44fe1d42a8c3a8c625d77ca
AtienoObwanda <atienoobwanda@gmail.com>
2022-03-27 09:07:37 +0300

    design and functionality done. Yet to add the actual recipes| Pop up| and an alert after a message has been sent

--- js/recipe.js
+++ js/recipe.js
@@ -1,15 +1,3 @@
-//Navbar OnScroll
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    
-    document.getElementById("navbar").style.background = "#ffffff";
-  } else {
-   
-    document.getElementById("navbar").style.background = "none";
-  }
-}
-//Recipe Cards Toggle effect
 $(document).ready(function(){
     $(".recipeImg").click(function(){
       $(".recipeImg").hide('1000');
@@ -20,4 +8,10 @@ $(document).ready(function(){
       $(".recipeImg").slideDown('1500');
     });
   })
-  
+
+
+
+
+
+
+                 
